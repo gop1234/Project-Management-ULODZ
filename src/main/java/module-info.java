8db2e4fec9c59519a1d5ddba80fd38b63a50654c
Gabriel <gabrielop2004@gmail.com>
2025-01-03 19:51:34 +0100

    Creation of project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module project_management.management {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens project_management.management to javafx.fxml;
+    exports project_management.management;
+}
---
+++ src/main/java/project_management/management/HelloController.java
@@ -0,0 +1,14 @@
+package project_management.management;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
