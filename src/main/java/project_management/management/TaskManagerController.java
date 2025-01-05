39792318a2df76b1dbe3d8c988f19e691e23d901
Javier <javieroutec2@gmail.com>
2025-01-05 15:54:55 +0100

    e

--- src/main/java/project_management/management/TaskManagerController.java
+++ src/main/java/project_management/management/TaskManagerController.java
@@ -1,10 +1,22 @@
 package project_management.management;
 
 import Data.Project;
+import Data.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 
 public class TaskManagerController {
 
     Project project;
+    @FXML
+    private Button add;
+    @FXML
+    private Button delete;
+    @FXML
+    private Button edit;
+
+
 
 
 }
