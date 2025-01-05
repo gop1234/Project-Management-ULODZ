bc6ba69e293c34f7bdf9ca0e8833bfa6e43927d0
Gabriel <gabrielop2004@gmail.com>
2025-01-05 15:54:32 +0100

    List that work in progress

--- src/main/java/project_management/management/TaskProgressController.java
+++ src/main/java/project_management/management/TaskProgressController.java
@@ -3,22 +3,35 @@ package project_management.management;
 import Data.Project;
 import Data.Task;
 import Data.Status;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.io.ObjectInputFilter;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class TaskProgressController {
 
     private Project project;
+    private Task selectedTask;
     @FXML
     private ListView<Task> toDo;
     @FXML
     private ListView<Task> onGoing;
     @FXML
     private ListView<Task> finished;
+    @FXML
+    private Label selectTask;
+
+    public void update(){
+        selectTask.setText("Selected task: " + selectedTask.getName());
+    }
+
     public void loadWindow(Project p){
         this.project = p;
         for(Task t : project.getTasks()){
@@ -30,6 +43,37 @@ public class TaskProgressController {
 
             }
         }
+
+        toDo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Task>() {
+            @Override
+            public void changed(ObservableValue<? extends Task> observable, Task oldValue, Task newValue) {
+                selectedTask=toDo.getSelectionModel().getSelectedItem();
+                toDo.getSelectionModel().clearSelection();
+                onGoing.getSelectionModel().clearSelection();
+                finished.getSelectionModel().clearSelection();
+                update();
+            }
+        });
+        onGoing.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Task>() {
+            @Override
+            public void changed(ObservableValue<? extends Task> observable, Task oldValue, Task newValue) {
+                selectedTask=onGoing.getSelectionModel().getSelectedItem();
+                toDo.getSelectionModel().clearSelection();
+                onGoing.getSelectionModel().clearSelection();
+                finished.getSelectionModel().clearSelection();
+                update();
+            }
+        });
+        finished.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Task>() {
+            @Override
+            public void changed(ObservableValue<? extends Task> observable, Task oldValue, Task newValue) {
+                selectedTask=finished.getSelectionModel().getSelectedItem();
+                toDo.getSelectionModel().clearSelection();
+                onGoing.getSelectionModel().clearSelection();
+                finished.getSelectionModel().clearSelection();
+                update();
+            }
+        });
     }
 
 }
